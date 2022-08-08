71e750f406c47dff663d0e5b8f34d2f910592a31
Thomas Tapia <thomastapia2001@gmail.com>
2022-08-08 09:46:59 -0500

    Prueba2

--- Proyecto/src/proyecto/Proyecto.java
+++ Proyecto/src/proyecto/Proyecto.java
@@ -14,7 +14,7 @@ public class Proyecto {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Prueba");
+        //System.out.println("Prueba");
     }
     
 }
